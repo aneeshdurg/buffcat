b12f397f9cef5593da615eb74968881e52e6f270
Aneesh Durg <aneeshdurg17@gmail.com>
2024-12-11 14:29:44 -0600

    Support limited memory mode

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::{File, OpenOptions};
 use std::io::{self, prelude::*, BufRead, BufWriter, SeekFrom, Write};
 use std::num::NonZeroUsize;
 use std::path::PathBuf;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use tqdm::pbar;
 
@@ -59,49 +59,134 @@ fn output_to_file(
     repeat_all: NonZeroUsize,
     files: &Vec<PathBuf>,
     nthreads: NonZeroUsize,
+    max_mem_usage: NonZeroUsize,
 ) -> Result<(), io::Error> {
-    let output = File::create(&output_file)?;
-
-    let mut buffers = Vec::new();
-    let mut total_len = 0;
-    for f in files {
-        // todo limit mem usage
-        let buf = Arc::new(std::fs::read(f)?);
-        total_len += buf.len() * usize::from(repeat_each);
-        buffers.push(buf);
+    let mut files: Vec<File> = files
+        .iter()
+        .map(|f| File::open(f))
+        .collect::<io::Result<Vec<_>>>()?;
+    let mut total_each = 0;
+    let mut prefix_lens = Vec::new();
+    let mut file_sizes = Vec::new();
+    for f in &files {
+        let bytes_from_file: usize = f.metadata()?.len() as usize;
+        file_sizes.push(bytes_from_file);
+        prefix_lens.push(total_each);
+        total_each += bytes_from_file;
     }
-    total_len *= usize::from(repeat_all);
-    let buffers = buffers;
+    prefix_lens.push(total_each);
+    let total_len = total_each * usize::from(repeat_each) * usize::from(repeat_all);
 
+    // Truncate the output file to the final size
+    let output = File::create(&output_file)?;
     output.set_len(total_len as u64)?;
     drop(output);
 
-    let (sender, receiver) = mpmc_queue::<(u64, Arc<Vec<u8>>)>(usize::from(nthreads) as u64 * 2);
+    let unlimited_mem = usize::from(max_mem_usage) > *prefix_lens.last().unwrap() as usize;
+    let buf_size = if unlimited_mem {
+        *file_sizes.iter().max().unwrap()
+    } else {
+        page_size::get()
+    };
+    assert!(
+        buf_size < max_mem_usage.into(),
+        "--max-mem-usage value must be at least the size of one page {}",
+        buf_size
+    );
+
+    let mem_usage = std::cmp::min(
+        *prefix_lens.last().unwrap() as usize,
+        usize::from(max_mem_usage),
+    );
+    let n_bufs = mem_usage / buf_size;
+    let (buffer_free_notify, buffer_free_events) =
+        mpmc_queue((n_bufs * usize::from(repeat_each)) as u64);
+    let queue_depth = usize::from(nthreads) as u64 * 2;
+    let (task_sender, task_recvr) = mpmc_queue::<(u64, usize, Arc<Vec<u8>>)>(queue_depth);
+
+    let mut buf_alloc_counts: Vec<usize> = Vec::new();
+    for i in 0..n_bufs {
+        // Initialize every buffer as ready to be written
+        buf_alloc_counts.push(1);
+        while let Err(_) = buffer_free_notify.try_send(i) {}
+    }
+
+    let pbar = Arc::new(Mutex::new(pbar(Some(total_len))));
+
     let mut children: Vec<JoinHandle<Result<(), io::Error>>> = vec![];
     for _ in 0..nthreads.into() {
-        let receiver = receiver.clone();
+        let buffer_free_notify = buffer_free_notify.clone();
+        let task_recvr = task_recvr.clone();
         let output = OpenOptions::new().write(true).open(&output_file)?;
         let mut output = BufWriter::new(output);
+        let pbar = pbar.clone();
         children.push(thread::spawn(move || {
-            for (offset, buffer) in receiver {
+            for (offset, buf_id, buffer) in task_recvr {
                 output.seek(SeekFrom::Start(offset))?;
                 output.write_all(&buffer)?;
+                let _ = pbar.lock().unwrap().update(buffer.len());
+                while let Err(_) = buffer_free_notify.try_send(buf_id) {}
             }
 
             Ok(())
         }));
     }
-    receiver.unsubscribe();
+    buffer_free_notify.unsubscribe();
+    task_recvr.unsubscribe();
 
-    let mut offset = 0;
-    let mut pbar = pbar(Some(total_len));
-    for _ in 0..repeat_all.into() {
-        for b in &buffers {
-            for _ in 0..repeat_each.into() {
-                while let Err(_) = sender.try_send((offset, b.clone())) {}
-                offset += b.len() as u64;
-                let _ = pbar.update(b.len());
+    let mut total_written = 0;
+    for buf_id in &buffer_free_events {
+        buf_alloc_counts[buf_id] -= 1;
+        if buf_alloc_counts[buf_id] == 0 {
+            let mut buf: Vec<u8> = std::iter::repeat(0).take(buf_size).collect();
+            for fid in 0..files.len() {
+                let f = &mut files[fid];
+                let foffset = f.seek(SeekFrom::Current(0))?;
+                let nbytes = f.read(&mut buf)?;
+                if nbytes == 0 {
+                    continue;
+                }
+
+                let buf = Arc::new(buf);
+                buf_alloc_counts[buf_id] = usize::from(repeat_each) * usize::from(repeat_all);
+                for i in 0..usize::from(repeat_all) {
+                    let mut offset = total_each * i + prefix_lens[fid] * usize::from(repeat_each);
+                    for _ in 0..usize::from(repeat_each) {
+                        while let Err(_) =
+                            task_sender.try_send((offset as u64 + foffset, buf_id, buf.clone()))
+                        {
+                        }
+                        offset += file_sizes[fid];
+                        total_written += buf.len();
+                    }
+                }
+
+                break;
             }
+        }
+
+        if total_written == total_len {
+            break;
+        }
+    }
+
+    let mut n_outstanding_buffers = buf_alloc_counts
+        .iter()
+        .fold(0, |acc, e| acc + if *e > 0 { 1 } else { 0 });
+    let evt_iter = &mut buffer_free_events.into_iter();
+    while n_outstanding_buffers > 0 {
+        match evt_iter.next() {
+            Some(buf_id) => {
+                buf_alloc_counts[buf_id] -= 1;
+                if buf_alloc_counts[buf_id] == 0 {
+                    n_outstanding_buffers -= 1
+                }
+
+                if n_outstanding_buffers == 0 {
+                    break;
+                }
+            }
+            None => break,
         }
     }
     Ok(())
@@ -135,6 +220,8 @@ fn main() -> Result<(), std::io::Error> {
             } else {
                 NonZeroUsize::new(num_cpus::get()).expect("Failed to get core count")
             },
+            args.max_mem_usage
+                .unwrap_or(NonZeroUsize::new(usize::max_value()).unwrap()),
         ),
         None => output_to_stdout(args.repeat_each, args.repeat_all, &args.files),
     }
